20d6e94ffe331c198ef354bd8e9c6d23eb03cc33
Oliver Wong <oliver@owiber.com>
2014-04-01 22:49:42 -0500

    move intern

--- node_modules/intern/tests/selftest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname $0)/.."
-node_modules/.bin/intern-client config=tests/selftest.intern selftest=true
-node_modules/.bin/intern-runner config=tests/selftest.intern selftest=true
